b14f8a0b5a27b8527a553ba6e43c259420d179cf
blinklv <blinklv@icloud.com>
2018-08-30 15:06:24 +0800

    Add TestQueuePop.

--- cache_test.go
+++ cache_test.go
@@ -39,3 +39,47 @@ func TestQueuePush(t *testing.T) {
 		assert.Equal(t, q._tailSize(), e.n%blockCapacity)
 	}
 }
+
+func TestQueuePop(t *testing.T) {
+	elements := []struct {
+		pushNumber int
+		popNumber  int
+		nilNumber  int
+	}{
+		{pushNumber: 1000, popNumber: 25},
+		{pushNumber: 1000, popNumber: 40},
+		{pushNumber: 2000, popNumber: 40},
+		{pushNumber: 0, popNumber: 10},
+		{pushNumber: 10 * blockCapacity, popNumber: 10},
+		{pushNumber: 10*blockCapacity + blockCapacity/2, popNumber: 11},
+	}
+
+	for _, e := range elements {
+		q := &queue{}
+		for i := 0; i < e.pushNumber; i++ {
+			q.push(index{})
+		}
+		assert.Equal(t, q.size(), e.pushNumber)
+		assert.Equal(t, q.bn, q._bn())
+
+		for i := 0; i < e.popNumber; i++ {
+			if q.pop() == nil {
+				e.nilNumber++
+			}
+		}
+
+		t.Logf("indices-number (%d) blocks-number (%d) nil-pop (%d)",
+			q.size(), q.bn, e.nilNumber)
+
+		bn := (e.pushNumber + blockCapacity - 1) / blockCapacity
+		if bn <= e.popNumber {
+			assert.Equal(t, q.bn, 0)
+			assert.Equal(t, q.size(), 0)
+			assert.Equal(t, e.nilNumber, e.popNumber-bn)
+		} else {
+			assert.Equal(t, q.bn, q._bn())
+			assert.Equal(t, q.bn, bn-e.popNumber)
+			assert.Equal(t, e.nilNumber, 0)
+		}
+	}
+}
