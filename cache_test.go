cc28de438e4692b80fdd5528a0d62bcc871ce835
blinklv <blinklv@icloud.com>
2018-08-30 13:11:34 +0800

    Add TestQueuePush.

---
+++ cache_test.go
@@ -0,0 +1,39 @@
+// cache_test.go
+//
+// Author: blinklv <blinklv@icloud.com>
+// Create Time: 2018-08-29
+// Maintainer: blinklv <blinklv@icloud.com>
+// Last Change: 2018-08-30
+
+package cache
+
+import (
+	"github.com/bmizerany/assert"
+	"testing"
+)
+
+func TestQueuePush(t *testing.T) {
+	elements := []struct {
+		n int
+	}{
+		{10},
+		{50},
+		{100},
+		{250},
+		{500},
+		{1000},
+		{2000},
+		{5000},
+	}
+
+	for _, e := range elements {
+		q := &queue{}
+		for i := 0; i < e.n; i++ {
+			q.push(index{})
+		}
+		t.Logf("indices-number (%d) actual-blocks-number (%d) tail-size (%d)",
+			e.n, q._bn(), q._tailSize())
+		assert.Equal(t, q._bn(), (e.n+blockCapacity-1)/blockCapacity)
+		assert.Equal(t, q._tailSize(), e.n%blockCapacity)
+	}
+}
